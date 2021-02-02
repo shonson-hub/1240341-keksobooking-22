b1b4b86be8f780d43d5e5c508e2c60e1c7758fc0
shonson-hub <shonborland@gmail.com>
2021-02-02 23:44:21 +0300

    Добавляем первые функции

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// let randomNumber = function (firstNumber, lastNumber) {
+//   let rand = firstNumber - 0.5 + Math.random() * (lastNumber - firstNumber + 1);
+
+//   if (firstNumber >= lastNumber) {
+//     console.log('Первое слово дороже второго');
+//   }
+//   return Math.round(rand);
+// }
+
+// console.log(randomNumber(115, 15));
+
+let randomNumber = function (firstNumber, lastNumber) {
+  let rand;
+  if (firstNumber < lastNumber) {
+    rand = firstNumber - 0.5 + Math.random() * (lastNumber - firstNumber + 1);
+  } else {
+    console.log('Первое слово дороже второго');
+  }
+  return Math.round(rand);
+}
+
+console.log(randomNumber(115, 15));
+
+let randomPartNumber = function (firstNumber, lastNumber,partNumber) {
+  let rand;
+  if (firstNumber < lastNumber) {
+    rand = (firstNumber - 0.5 + Math.random() * (lastNumber - firstNumber + 1)) * partNumber;
+  } else {
+    console.log('Первое слово дороже второго');
+  }
+  return rand;
+}
+
+console.log(randomPartNumber(5, 15, 1000));
+
+// Источник https://learn.javascript.ru/task/random-int-min-max
