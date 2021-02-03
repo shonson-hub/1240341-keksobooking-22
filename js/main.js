947b0d29dfd97b6e3ea6f400b48ab831218059dd
shonson-hub <shonborland@gmail.com>
2021-02-03 22:36:26 +0300

    Исправления теста

--- js/main.js
+++ js/main.js
@@ -14,23 +14,23 @@ let randomNumber = function (firstNumber, lastNumber) {
   if (firstNumber < lastNumber) {
     rand = firstNumber - 0.5 + Math.random() * (lastNumber - firstNumber + 1);
   } else {
-    console.log('Первое слово дороже второго');
+    return ('Первое слово дороже второго');
   }
   return Math.round(rand);
 }
 
-console.log(randomNumber(115, 15));
+alert(randomNumber(115, 15));
 
 let randomPartNumber = function (firstNumber, lastNumber,partNumber) {
   let rand;
   if (firstNumber < lastNumber) {
     rand = (firstNumber - 0.5 + Math.random() * (lastNumber - firstNumber + 1)) * partNumber;
   } else {
-    console.log('Первое слово дороже второго');
+    return('Первое слово дороже второго');
   }
   return rand;
 }
 
-console.log(randomPartNumber(5, 15, 1000));
+alert(randomPartNumber(5, 15, 1000));
 
 // Источник https://learn.javascript.ru/task/random-int-min-max
