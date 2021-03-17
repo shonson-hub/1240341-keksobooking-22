86944dfc0b8cd423183b71669dbe417cc6a0ce15
shontrope <shonborland@gmail.com>
2021-03-17 13:25:22 +0300

    просто правки

--- js/another-card.js
+++ js/another-card.js
@@ -9,49 +9,49 @@ const hideEmpty = (field, element) => field ? element.textContent = field : elem
 const createCard = ({offer,author}) => {
 
 
-    const newElement = cardTemplate.cloneNode(true);
+  const newElement = cardTemplate.cloneNode(true);
 
-    hideEmpty(offer.title, newElement.querySelector('.popup__title'));
-    hideEmpty(offer.type, newElement.querySelector('.popup__type'));
-    hideEmpty(offer.description, newElement.querySelector('.popup__description'));
-    const hidePrice = () => {
-      const popupPrice = newElement.querySelector('.popup__text--price');
-      if (offer.price) {
-        popupPrice.textContent = offer.price + ' ₽/ночь';
-      } else {
-        popupPrice.classList.add('hidden');
-      }
-    };
-    hidePrice();
+  hideEmpty(offer.title, newElement.querySelector('.popup__title'));
+  hideEmpty(offer.type, newElement.querySelector('.popup__type'));
+  hideEmpty(offer.description, newElement.querySelector('.popup__description'));
+  const hidePrice = () => {
+    const popupPrice = newElement.querySelector('.popup__text--price');
+    if (offer.price) {
+      popupPrice.textContent = offer.price + ' ₽/ночь';
+    } else {
+      popupPrice.classList.add('hidden');
+    }
+  };
+  hidePrice();
 
-    const hideRoomsGuests = () => {
-      const popupCapacity = newElement.querySelector('.popup__text--capacity');
-      if(offer.rooms, offer.guests) {
-        popupCapacity.textContent = offer.rooms + ' комнаты для ' + offer.guests + ' гостей';
-      } else {
-        popupCapacity.classList.add('hidden');
-      }
-    };
-    hideRoomsGuests();
+  const hideRoomsGuests = () => {
+    const popupCapacity = newElement.querySelector('.popup__text--capacity');
+    if(offer.rooms, offer.guests) {
+      popupCapacity.textContent = offer.rooms + ' комнаты для ' + offer.guests + ' гостей';
+    } else {
+      popupCapacity.classList.add('hidden');
+    }
+  };
+  hideRoomsGuests();
 
-    const hideTimeCheck = () => {
-      const popupTime = newElement.querySelector('.popup__text--time');
-      if (offer.checkin, offer.checkout) {
-        popupTime.textContent = 'Заезд после ' + offer.checkin + ', выезд до ' + offer.checkout;
-      } else {
-        popupTime.classList.add('hidden');
-      }
-    };
-    hideTimeCheck();
+  const hideTimeCheck = () => {
+    const popupTime = newElement.querySelector('.popup__text--time');
+    if (offer.checkin, offer.checkout) {
+      popupTime.textContent = 'Заезд после ' + offer.checkin + ', выезд до ' + offer.checkout;
+    } else {
+      popupTime.classList.add('hidden');
+    }
+  };
+  hideTimeCheck();
 
-    const hideAvatar = () => author.avatar ? newElement.querySelector('.popup__avatar').src = author.avatar : newElement.querySelector('.popup__avatar').classList.add('hidden');
-    hideAvatar();
+  const hideAvatar = () => author.avatar ? newElement.querySelector('.popup__avatar').src = author.avatar : newElement.querySelector('.popup__avatar').classList.add('hidden');
+  hideAvatar();
 
-    generateFeaturesElement(offer.features, newElement);
-    createPhotoElement(offer.photos, newElement);
+  generateFeaturesElement(offer.features, newElement);
+  createPhotoElement(offer.photos, newElement);
 
 
-    return newElement;
+  return newElement;
 
 };
 
